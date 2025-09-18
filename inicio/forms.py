26e42d0249af72732d35fb3e92f822417fa8a0aa
caroperezz194-sys <caroperezz194@gmail.com>
2025-09-18 20:46:45 -0300

    Update forms.py

--- inicio/forms.py
+++ inicio/forms.py
@@ -4,9 +4,9 @@ class FormularioCreacionProducto(forms.Form):
     nombre = forms.CharField(max_length=20)
     precio = forms.DecimalField(max_digits=10, decimal_places=2)
     descripcion = forms.CharField(
-        widget=forms.Textarea,  # permite escribir varias líneas
-        required=False,         # opcional si quieres permitir que quede vacío
-        max_length=1000,        # límite de caracteres (puedes ajustar)
+        widget=forms.Textarea,  
+        required=False,         
+        max_length=1000,        
         label='Descripción'
     )
     imagen = forms.ImageField(  
